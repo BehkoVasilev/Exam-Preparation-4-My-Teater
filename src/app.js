46054e763c099703c35550ab7c37816d05166d7e
BehkoVasilev <vencislavvsilev@gmail.com>
2022-12-02 14:39:43 +0200

    add createView

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import {page} from "./lib.js";
 import {render} from "./lib.js";
 import { getUserData } from "./util.js";
+import { showCreate } from "./views/createView.js";
 import { showHome } from "./views/homeView.js";
 import { showLogin } from "./views/loginVeiw.js";
 import { updateNav } from "./views/nav.js";
@@ -15,7 +16,7 @@ page("/", showHome);
 page("/home", showHome);
 page("/login", showLogin);
 page("/register", showRegister);
-page("/create", () => console.log("create"));
+page("/create", showCreate);
 page("/details/:id", () => console.log("details"));
 page("/edit/:id", () => console.log("edit"));
 page("/profil", () => console.log("profil"));
